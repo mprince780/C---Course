002b11b845e9275cf0f57a9047c0eece78761a48
mdprince <mprince780@gmail.com>
2022-07-27 02:47:54 +0530

    day4

---
+++ tut4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int glo =7;
+void add(){
+    int a;
+    cout<<glo;
+}
+
+int main(){
+    // int a=5;
+    // int b=6;
+    int glo=8;
+   glo=45;
+    int a=41,b=6;
+    int c=a+b;
+    bool d = false;
+    add();
+// cout<<"this is tut 4. \nthe value of a is " <<a<<  "\n the value of b is " <<b<< " \nthe value of c is " <<c;
+cout<<glo <<d;
+return 0;
+}
