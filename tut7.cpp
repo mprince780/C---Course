a28ccf91c3a6ec1fb496cc4724dea17658d214b0
mdprince <mprince780@gmail.com>
2022-07-28 09:36:17 +0530

    ok

---
+++ tut7.cpp
@@ -0,0 +1,28 @@
+ #include<iostream>
+
+using namespace std;
+
+int c=69;
+
+
+int main()
+{
+    // int num1, num2, c;
+    // cout<<" enter the value of num1  \n ";
+    // cin>> num1;
+
+    // cout<<" enter the value of num2  \n ";
+    // cin>> num2;
+    // c=num1+num2;
+    // cout<<" the sum is \n "<<c<<endl;
+    // cout<<"the global c is "<<::c;
+
+    float d=34.4f;
+    long double e = 34.4l;
+
+    cout<<"the value of d id "<<d<<endl<<"the value of e is "<<e;
+
+    return 0;
+}
+
+
