26cf0273801d3dac9d3d2076f6de3298791d48b4
mdprince <mprince780@gmail.com>
2022-07-29 10:37:29 +0530

    ok

---
+++ tut8.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    // int a=45;
+    // cout<<a;
+    // const int aa=67;
+    // cout<<a;
+    //  aa= 34;
+    // cout<<a;
+
+
+    // int a=3,b=56,c=7855;
+    // cout<<setw(4)<<a<<endl<<setw(4)<<b<<endl<<setw(4)<<c<<endl;
+    // cout<<a<<endl<<b<<endl<<c;
+
+    
+     return 0;
+}
