8bd5bf28c73e390e31774d8fb5238d8e02575e02
mdprince <mprince780@gmail.com>
2022-07-29 14:26:56 +0530

    g

---
+++ tut10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+// for (int i = 0; i <= 50; i++)
+// {
+//     /* code */
+//     cout<<i<<endl;
+//     //i++;
+// }
+
+//     return 0;
+
+
+}
