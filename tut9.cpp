7062d050e11afb8f781901adb927d3860815e89d
mdprince <mprince780@gmail.com>
2022-07-29 11:19:12 +0530

    ok

--- tut9.cpp
+++ tut9.cpp
@@ -7,19 +7,38 @@ int main()
     int age;
      cout<<"Tell me ur age"<<endl;
      cin>>age;
-     if ((age>18) && (age>0)){
-        cout<<"Allowed";
-     }
-     else if(age==18){
-        cout<<"kid";
-     }
-     else if (age<1)
-     {
-        cout<<"not born";
-     }
-     else{
-        cout<<"not allowed";
-     }
+    //  if ((age>18) && (age>0)){
+    //     cout<<"Allowed";
+    //  }
+    //  else if(age==18){
+    //     cout<<"kid";
+    //  }
+    //  else if (age<1)
+    //  {
+    //     cout<<"not born";
+    //  }
+    //  else{
+    //     cout<<"not allowed";
+    //  }
+
+    switch (age)
+    {
+    case  18:
+        cout<<"y r 18";
+        break;
+
+    case  22:
+        cout<<"y r 22";
+        break;
+
+    case  5:
+        cout<<"y r 5";
+        break;        
+    
+    default:
+        cout<<"no spl case";
+        break;
+    }
 
 
     return 0;
