ddd0e7d5419323524ea6709d69c34b147cf0ca38
mdprince <mprince780@gmail.com>
2022-08-01 16:01:21 +0530

    go

--- tut10.cpp
+++ tut10.cpp
@@ -11,7 +11,29 @@ int main()
 //     //i++;
 // }
 
-//     return 0;
+// int i=1;
+// while(i<=40){
+//     cout<<i<<endl;
+//     i++;
+//     }
 
+//     int i=1;
+// while(true){
+//     cout<<i<<endl;
+//     i++;
+//     }
 
+// int i=1;
+//  do{
+//     cout<<i<<endl;
+//     i++;
+//     }
+//     while(i<30);
+
+int i=1;
+do{
+    cout<<6*i<<endl;
+    i++;
+} while(i<11);
+    return 0;
 }
