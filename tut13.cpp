ca45bdaeb57ec31a735c66431e76537548c4fe32
mdprince <mprince780@gmail.com>
2022-08-03 15:52:48 +0530

    go

---
+++ tut13.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+int main( )
+{
+    int i=0;
+     int marks[]={23,45,65,45,76};
+     cout<<marks[2]<<endl;
+      marks[2]={34};
+     cout<<marks[2]<<endl<<"new"<<endl;
+
+    //  for (int i = 0; i < 5; i++)
+    //  {
+    //     cout<<marks[i]<<endl;
+    //  }
+
+    // while (i<=5)
+    // {
+    //     cout<<marks[i]<<endl;
+    //     i=i+1;
+    // }
+
+    do
+    {
+        cout<<marks[i]<<endl;
+        i++;
+    } 
+    while (i<5); 
+    return 0;
+}
