d23598cf0a64240b237b5a2972e6cd35ff89a53b
mdprince <mprince780@gmail.com>
2022-08-03 13:55:34 +0530

    go

--- tut12.cpp
+++ tut12.cpp
@@ -6,11 +6,13 @@ int main(int argc, char const *argv[])
 {
     int a = 13;
     int *b=&a;
+    int **c=&b;
 
     cout<<b<<endl;
     cout<<&a<<endl;
     cout<<&b<<endl;
-    cout<<*b;
+    cout<<*b<<endl;
+    cout<<c;
 
     return 0;
 }
