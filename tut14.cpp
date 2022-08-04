710678fe05d23d045df37ca9e9fc241ea911de4a
mdprince <mprince780@gmail.com>
2022-08-04 13:25:00 +0530

    go

--- tut14.cpp
+++ tut14.cpp
@@ -12,25 +12,46 @@ typedef struct employee
 
 int main()
 {
-    struct employee harry;
-    struct employee roahn;
-     harry.eId=1;
-     harry.favChar='d';
-     harry.salary=100000;
+//     struct employee harry;
+//     struct employee roahn;
+//      harry.eId=1;
+//      harry.favChar='d';
+//      harry.salary=100000;
 
-     roahn.eId=2;
-     roahn.favChar='g';
-     roahn.salary=200000;
+//      roahn.eId=2;
+//      roahn.favChar='g';
+//      roahn.salary=200000;
 
-     cout<<harry.eId<<endl;
-     cout<<harry.favChar<<endl;
-     cout<<harry.salary<<endl;
+//      cout<<harry.eId<<endl;
+//      cout<<harry.favChar<<endl;
+//      cout<<harry.salary<<endl;
 
-     cout<<roahn.eId<<endl;
-     cout<<roahn.favChar<<endl;
-     cout<<roahn.salary;
+//      cout<<roahn.eId<<endl;
+//      cout<<roahn.favChar<<endl;
+//      cout<<roahn.salary;
 
-    
-     
-    return 0;
+
+    union money
+    {
+    /* data */
+    int rice; //4
+    char car; //1
+    float pounds; //4
+    };
+
+
+        // union money m1;
+        // m1.rice = 34;
+        // m1.car='c';
+        // cout<<m1.rice;
+
+    enum Meal{brkfst, lunch,dinner};
+        Meal m1 = lunch;
+        Meal m2 = lunch;
+        cout<<(m1==1);
+        cout<<m2;   
+
+
+
+        return 0;
 }
